49611b0984a6232273f2fa9b642f828343b59941
Boris Kunda <34305099+BorisKunda@users.noreply.github.com>
2021-04-28 19:44:02 +0300

    popular movie list displayed

--- app/src/main/java/com/example/cmtvapp/adapter/MovieAdapter.kt
+++ app/src/main/java/com/example/cmtvapp/adapter/MovieAdapter.kt
@@ -1,4 +1,41 @@
 package com.example.cmtvapp.adapter
 
-class MovieAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.cmtvapp.R
+import com.example.cmtvapp.model.Movie
+
+class MovieAdapter : RecyclerView.Adapter<MovieAdapter.MovieViewHolder>() {
+
+    var movieMList: MutableList<Movie> = mutableListOf()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view: View = inflater.inflate(R.layout.movie_item_ll, parent, false)
+        return MovieViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        val movie = movieMList[position]
+        holder.apply {
+            titleTv.text = movie.title
+            //TODO: 4/28/2021 poster+star
+        }
+    }
+
+    override fun getItemCount(): Int = movieMList.size
+
+    /**ViewHolder*/
+    class MovieViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        val titleTv: TextView = itemView.findViewById(R.id.movie_title_tv)
+        val posterIv: ImageView = itemView.findViewById(R.id.movie_poster_iv)
+        val starIv: ImageView = itemView.findViewById(R.id.star_iv)
+
+    }
+
 }
--- app/src/main/java/com/example/cmtvapp/ui/fragments/MovieListFragment.kt
+++ app/src/main/java/com/example/cmtvapp/ui/fragments/MovieListFragment.kt
@@ -6,21 +6,27 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.cmtvapp.R
-import com.example.cmtvapp.utils.UtilMethods
+import com.example.cmtvapp.adapter.MovieAdapter
 import com.example.cmtvapp.viewmodel.MovieViewModel
 
 
 class MovieListFragment : Fragment() {
 
     private val movieViewModel: MovieViewModel by activityViewModels()
-
+    private lateinit var moviesListRV: RecyclerView
+    private lateinit var movieAdapter: MovieAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view: View = inflater.inflate(R.layout.fragment_movie_list, container, false)
+
+        buildUI(view)
+
         return view
     }
 
@@ -29,9 +35,30 @@ class MovieListFragment : Fragment() {
 
         movieViewModel.getPopularMovies().observe(viewLifecycleOwner, {
 
-
+            movieAdapter.movieMList = it.toMutableList()
+            movieAdapter.notifyDataSetChanged()
 
         })
+
+    }
+
+    private fun buildUI(view: View) {
+
+        setRecyclerView(view)
+
+    }
+
+    private fun setRecyclerView(view: View) {
+
+        movieAdapter = MovieAdapter()
+
+        moviesListRV = view.findViewById(R.id.movies_list_rv)
+
+        moviesListRV.apply {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(view.context)
+            adapter = movieAdapter
+        }
 
     }
 
